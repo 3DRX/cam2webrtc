c924b038c7f3c82012aec3a8441fd5597435bda8
3DRX <kjy20030228@icloud.com>
2024-11-02 17:19:04 +0800

    Init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, View, SafeAreaView, Button } from "react-native";
+import { mediaDevices, MediaStream, RTCView } from "react-native-webrtc";
 
 export default function App() {
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const start = async () => {
+    if (!stream) {
+      let s;
+      try {
+        s = await mediaDevices.getUserMedia({ video: true });
+        setStream(s);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <SafeAreaView>
+        {stream && <RTCView streamURL={stream.toURL()} />}
+        <View>
+          <Button title="Start" onPress={start} />
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
